98846d250fb47e3d8510a0e9876548944ad5c34a
Kyle Pfromer <kpfromer2@gmail.com>
2018-05-31 13:50:51 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+
+export default class App extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList />
+        </div>
+      </div>
+    );
+  }
+}
